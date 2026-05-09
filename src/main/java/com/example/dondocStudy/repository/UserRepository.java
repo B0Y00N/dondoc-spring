0d1bee0e402621553bddd824da9aef9e9548f557
B0Y00N <qpqp0109@gmail.com>
2026-05-09 18:22:44 +0900

    refactor: simplify post method codes using NamedParameterJdbcTemplate and SqlParameterSource

--- src/main/java/com/example/dondocStudy/repository/UserRepository.java
+++ src/main/java/com/example/dondocStudy/repository/UserRepository.java
@@ -3,46 +3,36 @@ package com.example.dondocStudy.repository;
 import com.example.dondocStudy.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class UserRepository{
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedJdbcTemplate;
 
     private final RowMapper<UserEntity> userRowMapper = new BeanPropertyRowMapper<>(UserEntity.class);
 
-    public List<UserEntity> findAll() { return jdbcTemplate.query("select * from users", userRowMapper); }
+    public List<UserEntity> findAll() { return namedJdbcTemplate.query("select * from users", userRowMapper); }
 
     public  UserEntity save(UserEntity userEntity) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(userEntity);
+
         String sql = """
                 INSERT INTO users (user_id, user_password, name, age, current_pig_level, current_house_level, monthly_income, target_expense_ratio) VALUES
-                (?, ?, ?, ?, ?, ?, ?, ?)
+                (:userId, :userPassword, :name, :age, :currentPigLevel, :currentHouseLevel, :monthlyIncome, :targetExpenseRatio)
                 """;
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
-
-            ps.setString(1, userEntity.getUserId());
-            ps.setString(2, userEntity.getUserPassword());
-            ps.setString(3, userEntity.getName());
-            ps.setInt(4, userEntity.getAge());
-            ps.setInt(5, userEntity.getCurrentPigLevel());
-            ps.setInt(6, userEntity.getCurrentHouseLevel());
-            ps.setLong(7, userEntity.getMonthlyIncome());
-            ps.setInt(8, userEntity.getTargetExpenseRatio());
-
-            return ps;
-        }, keyHolder);
+        namedJdbcTemplate.update(sql, param, keyHolder, new String[]{"id"});
 
         if(keyHolder.getKey() != null) {
             userEntity.setId(keyHolder.getKey().longValue());
