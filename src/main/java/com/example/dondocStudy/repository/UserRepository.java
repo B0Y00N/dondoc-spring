f57bedc7727f2802c735f1fbf0f22a0fb91c0c5d
B0Y00N <qpqp0109@gmail.com>
2026-05-10 15:31:53 +0900

    refactor: try another refactoring method using SimpleJdbcInsert

--- src/main/java/com/example/dondocStudy/repository/UserRepository.java
+++ src/main/java/com/example/dondocStudy/repository/UserRepository.java
@@ -3,12 +3,11 @@ package com.example.dondocStudy.repository;
 import com.example.dondocStudy.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,27 +15,21 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class UserRepository{
-    private final NamedParameterJdbcTemplate namedJdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     private final RowMapper<UserEntity> userRowMapper = new BeanPropertyRowMapper<>(UserEntity.class);
 
-    public List<UserEntity> findAll() { return namedJdbcTemplate.query("select * from users", userRowMapper); }
+    public List<UserEntity> findAll() { return jdbcTemplate.query("select * from users", userRowMapper); }
 
-    public  UserEntity save(UserEntity userEntity) {
+    public UserEntity save(UserEntity userEntity) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("users")
+                .usingGeneratedKeyColumns("id", "created_at");
+
         SqlParameterSource param = new BeanPropertySqlParameterSource(userEntity);
+        Number newId = jdbcInsert.executeAndReturnKey(param);
 
-        String sql = """
-                INSERT INTO users (user_id, user_password, name, age, current_pig_level, current_house_level, monthly_income, target_expense_ratio) VALUES
-                (:userId, :userPassword, :name, :age, :currentPigLevel, :currentHouseLevel, :monthlyIncome, :targetExpenseRatio)
-                """;
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        namedJdbcTemplate.update(sql, param, keyHolder, new String[]{"id"});
-
-        if(keyHolder.getKey() != null) {
-            userEntity.setId(keyHolder.getKey().longValue());
-        }
+        userEntity.setId(newId.longValue());
 
         return userEntity;
     }
